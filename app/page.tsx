db5fb09cfe935cd804942a900477f0d5667d4308
shencan03 <shencan03@gmail.com>
2026-06-07 17:09:23 +0300

    feat: enter brief description on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>Why bother making this website?</h1>
+      <main className="grid grid-cols-2">
+        <article className="my-12 grid gap-4 mx-16">
+          <h1 className="text-6xl underline underline-offset-3">Why bother making this website?</h1>
+          <p className="max-w-md text-2xl py-6">I am having difficulties finding ways to express myself, I am not a musician nor an artist, I am a software developer and at that I am an amateur at best. But still, I thought it's worth giving it a shot as a means to express my thoughts, my taste in various things and my <i>skills</i> in web development.</p>
+        </article>
       </main>
     </>
   )
