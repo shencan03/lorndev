05e9794ae907e46602e1095c77b737c1d49b5acf
shencan03 <shencan03@gmail.com>
2026-06-07 20:14:41 +0300

    feat: implement active page styling at navbar

---
+++ components/NavLinks.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+
+const NavbarItems = [
+  {href: "/", label: "home"},
+  {href: "/blog", label: "blog"},
+  {href: "/thoughts", label: "thoughts"},
+  {href: "/curation", label: "curation"}
+]
+
+export default function Navlinks() {
+  const isActive = usePathname()
+
+  return (
+    <div>
+      {NavbarItems.map(({href, label}) => {
+        const activePath = href === isActive
+
+        return (
+          <Link key={href} href={href} className={`hover:bg-[#353536] hover:rounded-xl px-6 py-1 ${activePath? 'bg-[#353536] rounded-xl' : ''}`}>{label}</Link>
+        )
+      })}
+    </div>
+  )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link"
+import NavLinks from "@/components/NavLinks"
 
 export default function Navbar() {
 	return (
 		<div className="flex justify-between text-2xl bg-[#282829] rounded-xl mx-auto px-20 gap-20 py-3 my-4">
-			<Link href="/" className="hover:bg-[#353536] hover:rounded-xl px-2 py-1">home</Link>
-			<Link href="/blog" className="hover:bg-[#353536] hover:rounded-xl px-2 py-1">blog</Link>
-			<Link href="/thoughts" className="hover:bg-[#353536] hover:rounded-xl px-2 py-1">thoughts</Link>
-			<Link href="/curation" className="hover:bg-[#353536] hover:rounded-xl px-2 py-1">curation</Link>
+			<NavLinks />
 		</div>
 	)
 }
