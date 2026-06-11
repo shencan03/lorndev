84e5c4127e1d55590e2e9ade4d8c00aa82ea620b
shencan03 <shencan03@gmail.com>
2026-06-11 21:51:27 +0300

    chore: center IndexCard.tsx

--- components/IndexCard.tsx
+++ components/IndexCard.tsx
@@ -12,7 +12,7 @@ type IndexCardProps = {
 
 export default function IndexCard({colCount, items}: IndexCardProps) {
   return (
-    <div className={`grid grid-cols-${colCount} gap-x-10 text-5xl`}>
+    <div className={`grid grid-cols-${colCount} gap-x-80 text-5xl gap-y-60 mt-20 bg-[#404041] px-4 rounded-2xl py-4`}>
       {items.map((item) => (
         <Link key={item.href} href={item.href}>{item.title}</Link>
       ))}
