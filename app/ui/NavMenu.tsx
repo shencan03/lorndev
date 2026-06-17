8a14d637028bcfd41fc57334ef21034458d07aad
shencan03 <shencan03@gmail.com>
2026-06-17 03:40:38 +0300

    fix: remove button wrapper on navbar from tabindex

--- app/ui/NavMenu.tsx
+++ app/ui/NavMenu.tsx
@@ -19,7 +19,7 @@ export default function NavMenu({
         {links.map((link) => {
           return (
             <>
-              <Button variant="link" className="text-white">
+              <Button variant="link" className="text-white" tabIndex={-1}>
                 <Link key={link.link} href={link.href} className="text-lg">
                   {link.link}
                 </Link>
