ab6d32a1b716a8593f0edd67d0ece4501b4ea7e1
shencan03 <shencan03@gmail.com>
2026-06-22 10:43:41 +0300

    fix: ts errors

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,7 +4,7 @@ export async function writeJsonResponse(res: JSON) {
   const data = JSON.stringify(res);
   try {
     await promises.writeFile(`${process.cwd()}/response.json`, data);
-  } catch (err) {
+  } catch (err: any) {
     throw new Error(err);
   }
 }
